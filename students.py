74a481d5770e0d6141e130431d49b761e0eee877
Sorin Gore <soringoredev@gmail.com>
2026-02-12 22:47:46 +0200

    MAke changes

--- students.py
+++ students.py
@@ -8,9 +8,3 @@ def create_student(name):
 def get_students():
     cursor.execute('SELECT * FROM students')
     return cursor.fetchall()
-
-def hakuna_matata():
-    print('zdarova')
-
-def hakuna_matata2():
-    print('zdarova2')
